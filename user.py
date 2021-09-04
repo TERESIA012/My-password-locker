64b71c0e714b568ea94e85f1e5dd05bb340afaf2
TERESIA012 <kingoriteresia@gmail.com>
2021-09-05 01:48:59 +0300

    initialize objects

--- user.py
+++ user.py
@@ -3,4 +3,10 @@ class User:
     class that generates new instance of user
     """
     
-    user_list=[] #an empty user list 
+    user_list=[] #an empty user list
+    
+    def __init__(self,username,password):
+    
+   
+        self.username=username
+        self.password=password
