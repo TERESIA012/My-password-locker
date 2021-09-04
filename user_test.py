3d103ffbcdd1af5398338618fb2d6c496f6723b6
TERESIA012 <kingoriteresia@gmail.com>
2021-09-05 01:59:40 +0300

    create setup method

--- user_test.py
+++ user_test.py
@@ -1,2 +1,10 @@
 import unittest #importing the unnitest
 from user import User #importing the user Class from the user module
+
+class TestUser(unittest.TestCase):
+
+    def setUp(self):
+        """
+        setUp method runs before the tests
+        """
+        self.new_user=User("Tess012","passWord") #create user object
