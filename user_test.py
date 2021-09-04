2db42d58e961fd0535b3456adcb50e76bcb58254
TERESIA012 <kingoriteresia@gmail.com>
2021-09-05 02:01:19 +0300

    test initialization

--- user_test.py
+++ user_test.py
@@ -8,3 +8,12 @@ class TestUser(unittest.TestCase):
         setUp method runs before the tests
         """
         self.new_user=User("Tess012","passWord") #create user object
+        
+    def test_init(self):
+        """
+        test_init test case to test the initialization
+        """
+    
+        self.assertEqual(self.new_user.username,"Tess012")
+        self.assertEqual(self.new_user.password,"passWord")    
+        
