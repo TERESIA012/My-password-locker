21f10cc1573cc0400d909727551821665ccab0a6
TERESIA012 <kingoriteresia@gmail.com>
2021-09-05 02:23:51 +0300

    function to generate new credentials

--- credentials.py
+++ credentials.py
@@ -5,3 +5,11 @@ class Credentials:
     """
     credentials_list=[]#an empty credentials list
     
+    def __init__(self,account,username,password):
+        """
+        new application and credentials are being generated
+        """
+        self.account=account
+        self.username=username
+        self.password=password
+    
