73498afe618bf37bee099512c0cff33d2efdfe4c
TERESIA012 <kingoriteresia@gmail.com>
2021-09-05 02:42:25 +0300

    setup method to run each Testcase

--- credentials_test.py
+++ credentials_test.py
@@ -1,3 +1,10 @@
 import unittest
 from credentials import Credentials #from credentials import class Credentials
 
+class TestCredentials(unittest.TestCase):
+    
+    def setUp(self):
+        """
+        setup method to run each Testcase
+        """
+        self.new_credentials=Credentials("facebook","terry012","passLock")
